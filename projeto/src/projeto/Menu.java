8cfc467006b43c71e689f46d1e8cf8afdca1316c
Letícia Queiroga <letqueirogaa@outlook.com>
2023-03-30 15:00:10 -0300

    Funções estoque

--- projeto/src/projeto/Menu.java
+++ projeto/src/projeto/Menu.java
@@ -1,18 +1,31 @@
 package projeto;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import projeto.model.Comida;
+import projeto.model.Produto;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import projeto.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner leia = new Scanner(System.in);
-		int opcao, tipo;
+		Produto produtos = new Produto();
+		ArrayList<Comida> lstComidas = new ArrayList<Comida>();
+		
+		int opcao = 0, tipo = 0, id = 0;
+		
+		
+		
 		while (true) {
 
-			System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-					+ "*****************************************************");
+			System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 			System.out.println("                                                     ");
 			System.out.println("                     Java Coffee                     ");
 			System.out.println("                                                     ");
@@ -25,7 +38,20 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
 
-			opcao = leia.nextInt();
+			try {// projeto
+				opcao = leia.nextInt();
+				if (opcao == 0) {
+					invalidMessage();
+				}
+				if (opcao > 2) {
+					invalidMessage();
+				}
+			} catch (InputMismatchException e) {
+				System.out.println(Cores.TEXT_YELLOW_BOLD + "*****************************************************");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Opção Inválida: Digite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
 
 			switch (opcao) {// cardapio ou estoque
 			case 1 -> {// cardapio
@@ -35,8 +61,9 @@ public class Menu {
 
 			case 2 -> {// estoque
 				while (true) {
-					System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
+ 					System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
 							+ "*****************************************************");
+					System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 					System.out.println("                                                     ");
 					System.out.println("                 Java Coffee Estoque                 ");
 					System.out.println("                                                     ");
@@ -52,76 +79,157 @@ public class Menu {
 					System.out.println("*****************************************************");
 					System.out.println("Entre com a opção desejada:                          ");
 					System.out.println("                                                     " + Cores.TEXT_RESET);
-					tipo = leia.nextInt();
+
+					try {// projeto
+						tipo = leia.nextInt();
+						if (tipo == 0) {
+							invalidMessage();
+						}
+						if (tipo > 6) {
+							invalidMessage();
+						}
+						
+					} catch (InputMismatchException e) {
+						System.out.println(Cores.TEXT_WHITE_BOLD + "\nOpção Inválida! Digite valores inteiros:\n");
+						leia.nextLine();
+						tipo = 0;
+						
+					}
 
 					switch (tipo) {// opções do estoque
 					case 1 -> {// Add itens
-						System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-								+ "*****************************************************");
+						System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 						System.out.println("                                                     ");
 						System.out.println("                  Adicionar itens:                   ");
 						System.out.println("                                                     ");
+						
+						// Instanciando o objeto de comida
+						Comida comida = new Comida();
+						
+						// Colocando variaveis comida
+						comida.setId(++id);
+						
+						System.out.println("Digite o nome: ");
+						comida.setNome(leia.next());
+						
+						System.out.println("Digite a quantidade do estoque: ");
+						comida.setQuantidade(leia.nextInt());
+						
+						System.out.println("Digite o preço unitário: ");
+						comida.setPreço(leia.nextFloat());
+						
+						System.out.println("Digite o tamanho (1 - Pequeno 2 - Grande): ");
+						comida.setTamanho(leia.nextInt());
+						
+						// Adicionando comida preenchida pelo usuário na lista de comida
+						lstComidas.add(comida);
+						
+						// Setando Lista de comidas dentro de Produtos
+						produtos.setComidas(lstComidas);
 
+						keyPress();
 					}
 
 					case 2 -> {// listar tudo
-						System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-								+ "*****************************************************");
+						System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 						System.out.println("                                                     ");
 						System.out.println("               Listar todos os itens:                ");
 						System.out.println("                                                     ");
+						
+						produtos.listarTodos();
 
+						keyPress();
 					}
 					case 3 -> {// buscar
-						System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-								+ "*****************************************************");
+						System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 						System.out.println("                                                     ");
 						System.out.println("                    Buscar itens:                    ");
 						System.out.println("                                                     ");
+						
+						System.out.println("Digite o id do produto: ");
+						produtos.buscarVisualizar(leia.nextInt());						
 
+						keyPress();
 					}
 					case 4 -> {// editar
-						System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-								+ "*****************************************************");
+						System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 						System.out.println("                                                     ");
 						System.out.println("                    Editar itens:                    ");
 						System.out.println("                                                     ");
+						
+						System.out.println("Editar comida(1) ou bebida(2)?");
+						int opcaoEditar = leia.nextInt();
+						
+						System.out.println("Digite o id que deseja buscar: ");					
+						if(opcaoEditar == 1) {
+							produtos.buscarComidaEditar(leia.nextInt());							
+						} else {
+							produtos.buscarBebidaEditar(leia.nextInt());
+						}
+						
+
+
+						keyPress();
 
 					}
 					case 5 -> {// remover
-						System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-								+ "*****************************************************");
+						System.out.println(Cores.TEXT_YELLOW + "*****************************************************");
 						System.out.println("                                                     ");
 						System.out.println("                    Apagar itens:                    ");
 						System.out.println("                                                     ");
 
-					}
-					case 6 -> {// sair
+						System.out.println("Apagar comida(1) ou bebida(2)?");
+						int opcaoApagar = leia.nextInt();
+						
+						System.out.println("Digite o id que deseja deletar: ");					
+						if(opcaoApagar == 1) {
+							produtos.deletarComida(leia.nextInt());							
+						} else {
+							// produtos.buscarBebidaEditar(leia.nextInt());
+						}
+
+						keyPress();
 
 					}
-					default -> {
-						System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-								+ "*****************************************************");
-						System.out.println("                  Opção Inválida!                    ");
+					case 6 -> {// sair
+						sobre();
+						leia.close();
+						System.exit(0);
 					}
+					
+
 					}
 				}
 			}
-			default -> {
-				System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND
-						+ "*****************************************************");
-				System.out.println("             Opção Inválida!                ");
+		
+
 			}
-			}
+		
+			
 		}
 
 	}
 
 	public static void sobre() {// participantes
-		System.out.println("Cafeteria");
+		System.out.println(Cores.TEXT_WHITE_BOLD + "*****************************************************");
+		System.out.println("                   Projeto Java Coffee                  ");
+		System.out.println("*****************************************************");
+		System.out.println("                                                     ");
 		System.out.println("Camille Bueno: https://github.com/CamillePB");
-	
+
+	}
+
+	public static void keyPress() {// parar o menu até apertar enter
+		try {
+			System.out.println(Cores.TEXT_RESET + "Pressione Enter para continuar...");
+			System.in.read();// tecla enter
+		} catch (IOException e) {
+			System.out.println("Erro de digitação");
+		}
 	}
 	
+	public static void invalidMessage() {//mensagem de erro 
+		System.out.println(Cores.TEXT_WHITE_BOLD +"\nOpção Inválida! Tente novamente:\n");
+	}
 
 }
--- projeto/src/projeto/model/Produto.java
+++ projeto/src/projeto/model/Produto.java
@@ -1,56 +1,144 @@
 package projeto.model;
 
-public abstract class Produto {
-private int id;
-private String nome;
-private int quantidade;
-private float preço;
+import java.util.ArrayList;
 
-public Produto(int id, String nome, int quantidade, float preço) {
-	this.id = id;
-	this.nome = nome;
-	this.quantidade = quantidade;
-	this.preço = preço;
-}
-
-public int getId() {
-	return id;
-}
-
-public void setId(int id) {
-	this.id = id;
-}
-
-public String getNome() {
-	return nome;
-}
-
-public void setNome(String nome) {
-	this.nome = nome;
-}
-
-public int getQuantidade() {
-	return quantidade;
-}
-
-public void setQuantidade(int quantidade) {
-	this.quantidade = quantidade;
-}
-
-public float getPreço() {
-	return preço;
-}
-
-public void setPreço(float preço) {
-	this.preço = preço;
-}
-public void visualizar() {
+public class Produto {
 	
-	System.out.println("            ID do produto : " + this.id);
-	System.out.println("            Nome do produto escolido : " + this.nome);
-	System.out.println("            Quantidade de produtos escolhidos : " + this.quantidade);
-	System.out.println("            Total do pedido : " + this.preço);
-	
-}
+	private int id;
+	private String nome;
+	private int quantidade;
+	private float preço;
+	private ArrayList<Comida> comidas = new ArrayList<Comida>();
+	private ArrayList<Bebida> bebidas = new ArrayList<Bebida>();
 
+	/*
+	public Produto(int id, String nome, int quantidade, float preço) {
+		this.id = id;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preço = preço;
+	}*/
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public float getPreço() {
+		return preço;
+	}
+
+	public void setPreço(float preço) {
+		this.preço = preço;
+	}
+
+	public ArrayList<Comida> getComidas() {
+		return comidas;
+	}
+
+	public void setComidas(ArrayList<Comida> comidas) {
+		this.comidas = comidas;
+	}
+	
+	public void listarTodos() {
+		
+		if(comidas.size() > 0) {
+			System.out.println("-------------------- Comidas --------------------");
+			for (var item : comidas)
+				item.visualizar();
+				System.out.println("\n");
+				
+			System.out.println("-------------------------------------------------\n");	
+		}
+		
+		if(comidas.size() > 0) {
+			System.out.println("-------------------- Bebidas --------------------");
+			for (var item : bebidas)
+				item.visualizar();
+				System.out.println("\n");
+				
+			System.out.println("-------------------------------------------------");	
+		}
+	}
+	
+	public void buscarVisualizar(int id) {
+		
+		// Percorrendo comidas procurando o id
+		for (var item : comidas) {
+			if (item.getId() == id) {
+				item.visualizar();
+				break;
+			}
+		}
+		
+		// Percorrendo bebidas procurando o id
+		for (var item : bebidas) {
+			if (item.getId() == id) {
+				item.visualizar();
+				break;	
+			}
+		}		
+	}
+	
+	public Comida buscarComidaEditar(int id) {
+		
+		// Percorrendo comidas procurando o id para editar
+		for (var item : comidas) {
+			if (item.getId() == id) {
+				item.editar();				
+				return item;
+			}
+		}		
+		return null;
+	}
+	
+	public void buscarBebidaEditar(int id) {
+		
+		// Percorrendo bebidas procurando o id para editar
+		for (var item : bebidas) {
+			if (item.getId() == id) {
+				//item.editar();
+				break;	
+			}
+		}
+	}
+	
+	public void deletarComida(int id) {
+		
+		// Percorrendo comidas procurando o id para deletar
+		for (var item : comidas) {
+			if (item.getId() == id) {
+				getComidas().remove(item);
+				return;
+			}
+		}	
+	}
+
+	public void visualizar() {
+
+		System.out.println("            ID do produto : " + this.id);
+		System.out.println("            Nome do produto escolido : " + this.nome);
+		System.out.println("            Quantidade de produtos escolhidos : " + this.quantidade);
+		System.out.println("            Total do pedido : " + this.preço);
+
+	}
 }
